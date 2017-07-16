e80b519bbd024d003cf74881dbfc22906e02dab2
ImJustToNy <imtony@protonmail.com>
2017-07-16 19:51:58 +0200

    Fixed eslint

--- migrations/1500121793015_posts.js
+++ migrations/1500121793015_posts.js
@@ -1,23 +1,23 @@
 exports.up = (pgm) => {
-    pgm.createTable('posts', {
-        id: 'id',
-        post_id: {
-            type: 'varchar',
-            unique: true,
-            notNull: true
-        },
-        comment_id: {
-            type: 'varchar',
-            unique: true,
-            notNull: true
-        },
-        created_at: {
-            type: 'timestamp',
-            default: pgm.func('NOW()')
-        }
-    })
-};
+  pgm.createTable('posts', {
+    id: 'id',
+    post_id: {
+      type: 'varchar',
+      unique: true,
+      notNull: true
+    },
+    comment_id: {
+      type: 'varchar',
+      unique: true,
+      notNull: true
+    },
+    created_at: {
+      type: 'timestamp',
+      default: pgm.func('NOW()')
+    }
+  })
+}
 
 exports.down = (pgm) => {
-    pgm.dropTable('posts')
-};
+  pgm.dropTable('posts')
+}
