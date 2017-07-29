386266dcf13a95bd73136be27e5affb8344f97eb
ImJustToNy <imtony@protonmail.com>
2017-07-29 15:09:31 +0200

    Added ability to check for country code in replaced url

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ setInterval(() => {
       lastChecked = posts[0].name
 
       async.every(posts, (post, callback) => {
-        if (post.domain === 'gfycat.com' && post.url.includes('gifs/detail/')) {
+        if (post.domain === 'gfycat.com' && /(\/[a-z][a-z])?\/gifs\/detail/g.test(post.url)) {
           post.fetch().comments.map(comment => comment.author.name).then(participants => {
             callback(null, true)
             if (!participants.includes(process.env.REDDIT_USERNAME)) {
               console.log(chalk.red(chalk.bold('Found new post: ') + post.title + ' [/r/' + post.subreddit.display_name + ']'))
 
-              post.reply('[Proper Gfycat URL](' + post.url.replace('gifs/detail/', '') + ') \n\n' + '^^I\'m ^^just ^^a ^^bot, ^^bleep, ^^bloop. [^^[Why?]](https://gist.github.com/ImJustToNy/cb3457e36f22123eb93864f0af639da3) [^^[Source ^^code]](https://github.com/ImJustToNy/GfycatDetailsConvert)')
+              post.reply('[Proper Gfycat URL](' + post.url.replace(/(\/[a-z][a-z])?\/gifs\/detail/g, '') + ') \n\n' + '^^I\'m ^^just ^^a ^^bot, ^^bleep, ^^bloop. [^^[Why?]](https://gist.github.com/ImJustToNy/cb3457e36f22123eb93864f0af639da3) [^^[Source ^^code]](https://github.com/ImJustToNy/GfycatDetailsConvert)')
             }
           })
         }
